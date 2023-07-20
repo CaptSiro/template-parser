ec5419e9f38b2cb263b22a09747a7476d31f82e5
CaptSiro <honzahorak433@icloud.com>
2023-07-20 13:42:11 +0200

    Created templating

--- out/index.js
+++ out/index.js
@@ -1,12 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const Tokenizer_1 = require("./tokenizer/Tokenizer");
-const Parser_1 = require("./parser/Parser");
-const t = new Tokenizer_1.Tokenizer("[{BPM}] \\{ {TITLE} \\}");
-console.log(t.getTokens());
-const p = new Parser_1.Parser("[{BPM}] { \\{ {TITLE} \\}", [
+const TemplateParser_1 = require("./parser/TemplateParser");
+const template_1 = __importDefault(require("./template"));
+// const t = new TemplateTokenizer("[{BPM}] \\{ {TITLE} \\}");
+// console.log(t.getTokens());
+const p = new TemplateParser_1.TemplateParser("[{BPM}] {ARTIST} - {TITLE}", [
     "BPM",
-    "TITLE"
+    "TITLE",
+    "ARTIST"
 ]);
-console.log(p.getTemplateConfig());
+const conf = p.getTemplateConfig();
+if (conf.type === "error") {
+    console.error(conf.error.message);
+}
+else {
+    const accessor = ident => {
+        return ident[0] + ident.substring(1).toLowerCase();
+    };
+    console.log((0, template_1.default)(conf.config, accessor));
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import { Tokenizer, Tokens } from "./tokenizer/Tokenizer";
-import { Parser } from "./parser/Parser";
+import { TemplateTokenizer, Tokens } from "./tokenizer/TemplateTokenizer";
+import { TemplateParser } from "./parser/TemplateParser";
+import template, { IdentAccessor } from "./template";
 
-const t = new Tokenizer("[{BPM}] \\{ {TITLE} \\}");
-console.log(t.getTokens());
+// const t = new TemplateTokenizer("[{BPM}] \\{ {TITLE} \\}");
+// console.log(t.getTokens());
 
-const p = new Parser("[{BPM}] \\{ {TITLE} \\}", [
+const p = new TemplateParser("[{BPM}] {ARTIST} - {TITLE}", [
     "BPM",
-    "TITLE"
+    "TITLE",
+    "ARTIST"
 ]);
-console.log(p.getTemplateConfig());
+
+const conf = p.getTemplateConfig();
+
+if (conf.type === "error") {
+    console.error(conf.error.message);
+} else {
+    const accessor: IdentAccessor = ident => {
+        return ident[0] + ident.substring(1).toLowerCase();
+    }
+
+    console.log(template(conf.config, accessor));
+}
